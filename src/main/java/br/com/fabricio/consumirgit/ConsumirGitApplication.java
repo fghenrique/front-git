f3774f2241823ca6d8b43c0d022365750687c64a
fghenrique <fabricio.henrique@fatec.sp.gov.br>
2020-09-21 20:31:56 -0300

    Start projeto

---
+++ src/main/java/br/com/fabricio/consumirgit/ConsumirGitApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabricio.consumirgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumirGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumirGitApplication.class, args);
+    }
+
+}
