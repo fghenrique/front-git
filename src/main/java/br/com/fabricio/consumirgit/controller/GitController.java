1ecfbed556f59ab8af8167b13d9b0bb6e311916a
fghenrique <fabricio.henrique@fatec.sp.gov.br>
2020-09-23 01:31:51 -0300

    Ajustando lsita de Programadores

---
+++ src/main/java/br/com/fabricio/consumirgit/controller/GitController.java
@@ -0,0 +1,42 @@
+package br.com.fabricio.consumirgit.controller;
+
+import br.com.fabricio.consumirgit.model.Programador;
+import br.com.fabricio.consumirgit.service.ProgramadoresService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Objects;
+
+@Controller
+@RequestMapping("programadores")
+public class GitController extends Base<Programador> {
+
+    public GitController() {
+        this.URL_LIST = "public/list/list_programadores";
+        this.URL_FORM = "public/form/form_programadores";
+        this.TITLE_LIST = "Lista de programadores";
+        this.TITLE_NEW = "Novo programadores";
+        this.TITLE_EDIT = "Editar programadores";
+        this.SINGLE_OBJECT = "programador";
+        this.FORM_OBJECT_NAME = "Programadores";
+        this.LIST_OBJECT = "programadores";
+        this.SUCCESS_MESSAGE = "Programadores cadastrada com sucesso!";
+        this.EDITABLE_MESSAGE = "Programadores alterado com sucesso!";
+    }
+
+    @Autowired
+    private ProgramadoresService prsService;
+
+    private List<Programador> listaProgramador = null;
+
+    @GetMapping("/list")
+    public ModelAndView listar() {
+        if (Objects.isNull(listaProgramador))
+            listaProgramador = prsService.findAll().getItems();
+        return listPage(listaProgramador);
+    }
+}
---
+++ src/main/java/br/com/fabricio/consumirgit/model/Programador.java
@@ -0,0 +1,61 @@
+package br.com.fabricio.consumirgit.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Programador {
+    private String id;
+    private String login;
+    private String url;
+    private String html_url;
+    private String score;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public void setHtml_url(String html_url) {
+        this.html_url = html_url;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
